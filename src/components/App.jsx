b4475820764b19af8278a7bb355f46779a430c2b
Chris Joyce <chrisj@benon.com>
2020-01-05 10:31:10 +1000

    Add signOut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class App extends Component {
+
+    signOut() {
+        firebaseApp.auth().signOut();
+    }
+
+
     render() {
         return (
-            <div>App</div>
+            <div style={{ margin: '5px' }}>
+                <h3>Goal Coach</h3>
+                <hr />
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.signOut()}
+                >
+                    Sign Out
+            </button>
+            </div>
         )
     }
 }
+
 export default App;
